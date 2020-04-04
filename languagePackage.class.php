5a48dad38c52ecde27f754282efef019ed25a19e
nfa04 <aeby.noel@gmail.com>
2020-04-04 13:03:26 +0200

    Initial Commit

---
+++ languagePackage.class.php
@@ -0,0 +1,92 @@
+<?php
+error_reporting (E_ALL ^ E_NOTICE);
+class languagePackage {
+	
+	private $packageDir;
+	private $idList;
+	private $reader;
+	
+	function __construct($dir, $reader) {
+		$this->packageDir = $dir;
+		$this->reader = $reader;
+		$this->read();
+	}
+	
+	function read() {
+		// Scan directory
+		
+		$scan = scandir($this->packageDir) or die('Could not read package directory');
+		
+		$list = array();
+		
+		foreach($scan AS $result) {
+			if($result != '.' AND $result != '..') $list[] = str_replace('.txt','',$result);
+		}
+		
+		$this->idList = $list;
+		
+	}
+	
+	function getStringRessourcePath($stringId) {
+		
+		if(in_array($stringId, $this->idList)) {
+			// Perfect. This string exists so return path
+			$pdir = $this->packageDir;
+			return $pdir.'/'.$stringId.'.txt';
+		}
+		else {
+			// This string doesn't exist return false
+			return false;
+		}
+	}
+	
+	function getString($stringId) {
+		// Read this ressource if it exists
+		$resPath = $this->getStringRessourcePath($stringId);
+		if($resPath !== false) return $this->renderString(file_get_contents($resPath));
+		else return false;
+	}
+	
+	function setString($stringId, $value, $override = true) {
+		if($override) {
+			// Override is enabled so write data anyway
+			file_put_contents($this->packageDir.'/'.$stringId.'.txt', $value);
+		}
+		else {
+			// Override is disabled so check if data exist
+			if($this->getStringRessourcePath($stringId) === false) {
+				// No data exist. Write them
+				file_put_contents($this->packageDir.'/'.$stringId.'.txt', $value);
+			}
+			else {
+				// Data exist. Don't write anything
+				return false;
+			}
+		}
+	}
+	
+	function printString($stringId) {
+		echo $this->getString($stringId);
+	}
+	
+	private function renderString($string) {
+		$words = explode(' ', $string);
+		for ($i = 0; $i <= count($words); $i++) {
+			if(substr($words[$i],0,1) == '@') {
+				// This is a command
+				$command = explode(':', $words[$i]);
+				// $command[0] contains the command
+				// $command[1] contains the parameters
+				if($command[0] == '@link') {
+					$data = explode('/', $command[1]);
+					// $data[0] contains the package
+					// $data[1] contains the stringId
+					$words[$i] = $this->reader->getLanguagePackage($data[0])->getString($data[1]);
+				}
+			} else continue;
+		}
+		return implode(' ', $words);
+	}
+
+}
+?>
---
+++ languageReader.class.php
@@ -0,0 +1,98 @@
+<?php
+class languageReader {
+
+	private $langPackages = array();
+	
+	private function is_complex($lang) {
+		if(strpos($lang, '-') === false) return false;
+		else return true;
+	}
+	
+	function __construct($langPackDir, $fallbackLanguage = 'en', $use_downgrade_fallbacks = true) {
+        define('langPackDir', $langPackDir);
+        define('fallbackLanguage', $fallbackLanguage);
+        define('use_downgrade_fallbacks', $use_downgrade_fallbacks);
+
+		$this->scan();
+    }
+	
+	function scan() {
+		$langPackageScan = scandir(langPackDir) or die('ERROR: Could not find language Packages in folder '.langPackDir);
+		foreach($langPackageScan AS $result) {
+			if ($result === '.' or $result === '..') continue;
+
+			if (is_dir(langPackDir . '/' . $result)) {
+				// Found package
+				$this->langPackages[] = $result;
+			}
+		}
+	}
+	
+	
+	function getFallbackPackage() {
+		return new languagePackage(langPackDir.'/'.fallbackLanguage, $this);
+	}
+	
+	function getDowngradeLanguageName($lang) {
+		return substr($lang, 0, strpos($lang, '-'));
+	}
+	
+	function getDowngradeLanguagePackage($lang) {
+		return new languagePackage(langPackDir.'/'.$this->getDowngradeLanguageName($lang));
+	}
+	
+	function getLanguagePackage($lang, $use_fallback = true) {
+		if(in_array($lang, $this->langPackages)) {
+			// Perfect. This package exists
+			return new languagePackage(langPackDir.'/'.$lang, $this);
+		}
+		else if(use_downgrade_fallbacks AND $this->is_complex($lang)) {
+			// This package doesn't exist so check if downgrade is available
+			return $this->getDowngradeLanguagePackage($lang);
+		}
+		else if($use_fallback) {
+			// This package doesn't exist and there isn't a downgrade available so return the fallback package
+			return $this->getFallbackPackage();
+		}
+		else return false;
+	}
+	
+	function getAutodetectedLanguagePackage() {
+		$accepted = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+		$weights = array();
+		foreach($accepted AS $lang) {
+			$lang = trim($lang);
+			if(strpos($lang, ';') === false) $weights[$lang] = 1;
+			else {
+				$explode = explode(';q=', $lang);
+				$weights[$explode[0]] = $explode[1];
+			}
+		}
+		arsort($weights);
+		$priorities = array_keys($weights);
+		foreach($priorities AS $lang) {
+			$langPack = $this->getLanguagePackage($lang, false);
+			if($langPack !== false) {
+				return $langPack;
+				break;
+			}
+		}
+		return $this->getFallbackPackage();
+	}
+	
+	function getIntranslatablePackage() {
+		return new languagePackage(langPackDir.'/intranslatable', $this);
+	}
+	
+	function getPackageList() {
+		return $this->langPackages;
+	}
+	
+	function getPackages() {
+		$packages = array();
+		foreach($this->langPackages AS $package) {
+			$packages[] = new languagePackage(langPackDir.'/'.$package, $this);
+		}
+		return $packages;
+	}
+}
